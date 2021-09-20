c4ea1be9e46ca6b76ddc00efa15465ba6a2476af
yarymaks <yarygin45@gmail.com>
2021-09-20 20:22:32 +0200

    add

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
 export default function App() {
+  const [value, setValue] = useState('');
+  const inputHandler = text => setValue(text);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>I love technology!</Text>
+      <KeyboardAvoidingView
+        behavior={Platform.OS == 'ios' ? 'padding' : 'height'}
+      >
+        <TextInput
+          placeholder="Text please"
+          value={value}
+          onChangeText={inputHandler}
+        />
+      </KeyboardAvoidingView>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +36,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'red',
+    fontSize: 32,
+  },
+  desc: {
+    marginBottom: 15,
+    color: 'green',
+    fontSize: 25,
   },
 });
